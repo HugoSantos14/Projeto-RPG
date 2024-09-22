ff8f721b4b5d0f3708b8f6a9dd9d3c1bb3d21147
Gabriel <159343630+GabrielEliDias@users.noreply.github.com>
2024-09-22 18:32:00 -0400

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -1,92 +1,127 @@
 import java.util.Scanner;
 
-public class Menu {
+class Menu {
     private static int pontos = 50;
-    private static int i = 0;
 
     public static void CriarPersonagem() {
         Scanner input = new Scanner(System.in);
-        Boolean selectedOption = false;
 
-        System.out.println("\t<<<Criação de personagem>>>\n");
+        System.out.println("\t<<<Diga seu nome!>>>\n");
         System.out.print("Nome: ");
-        // String nome = input.nextLine();
         Jogador jogador = new Jogador(input.nextLine(), 0, 0, 0, 0, 0);
         Console.clear();
-        
-        for (i = 0; selectedOption == false; i++) {
-            System.out.println("\t<<<Criação de personagem>>>\n");
+
+        do{
+            System.out.println("\t<<<Atribuir pontos de atributo>>>\n");
             System.out.println("Nome: " + jogador.nome);
             System.out.printf("\nDistribua seus atributos: (%d pontos restantes)\n\n", pontos);
-            switch (i + 1) {
+            System.out.println("1°-Força: " + jogador.getForça());
+            System.out.println("2°-Resistência: " + jogador.getResistência());
+            System.out.println("3°-Agilidade: " + jogador.getAgilidade());
+            System.out.println("4°-Destreza: " + jogador.getDestreza());
+            System.out.println("5°-Inteligência: " + jogador.getInteligência());
+            System.out.print("6°-Prosseguir na criação do personagem!\n>");
+            System.out.print("Qual atributo deve ser modificado:");
+
+            int Escolha = input.nextInt();
+            Console.clear();
+
+            switch(Escolha) {
                 case 1:
-                    System.out.print("Força: ");
-                    jogador.setForça(input.nextInt());
-                    pontos -= jogador.getForça();
+                    System.out.print(">Força: ");
+                    int NPontosF = input.nextInt();
+                    if (NPontosF > pontos) {
+                        System.out.println("Erro ao direcionar os pontos.");
+                    } else {
+                        jogador.setForça(NPontosF);
+                        pontos -= jogador.getForça();}
                     break;
-                        
                 case 2:
-                    jogador.exibirAtributos();
-                    System.out.print("Resistência: ");
-                    jogador.setResistência(input.nextInt());
-                    pontos -= jogador.getResistência();
+                    System.out.print(">Resistência: ");
+                    int NPontosR = input.nextInt();
+                    if (NPontosR > pontos) {
+                        System.out.println("Erro ao direcionar os pontos.");
+                    } else {
+                        jogador.setResistência(NPontosR);
+                        pontos -= jogador.getResistência();}
                     break;
-                        
                 case 3:
-                    jogador.exibirAtributos();
-                    System.out.print("Agilidade: ");
-                    jogador.setAgilidade(input.nextInt());
-                    pontos -= jogador.getAgilidade();
+                    System.out.print(">Agilidade: ");
+                    int NPontosA = input.nextInt();
+                    if (NPontosA > pontos) {
+                        System.out.println("Erro ao direcionar os pontos.");
+                    } else {
+                        jogador.setAgilidade(NPontosA);
+                        pontos -= jogador.getAgilidade();}
                     break;
-                        
                 case 4:
-                    jogador.exibirAtributos();
-                    System.out.print("Destreza: ");
-                    jogador.setDestreza(input.nextInt());
-                    pontos -= jogador.getDestreza();    
+                    System.out.print(">Destreza: ");
+                    int NPontosD = input.nextInt();
+                    if (NPontosD > pontos) {
+                        System.out.println("Erro ao direcionar os pontos.");
+                    } else {
+                        jogador.setDestreza(NPontosD);
+                        pontos -= jogador.getDestreza();}
                     break;
-                        
                 case 5:
-                    jogador.exibirAtributos();
-                    System.out.print("Inteligência: ");
-                    jogador.setInteligência(input.nextInt());
-                    pontos -= jogador.getInteligência();
+                    System.out.print(">Inteligência: ");
+                    int NPontosI = input.nextInt();
+                    if (NPontosI > pontos) {
+                        System.out.println("Erro ao direcionar os pontos.");
+                    } else {
+                        jogador.setInteligência(NPontosI);
+                        pontos -= jogador.getInteligência();}
                     break;
-
-                case 6:
-                    jogador.exibirAtributos();
-                    System.out.println("\nEscolha sua arma:\n");
-                    System.out.println("1 - Claymore (Pesada)\t2 - Sabre (Leve)\t3 - Cajado\n> ");
-                    switch (input.nextInt()) {
-                        case 1:
-                            jogador.setArma(new Arma("Claymore", "Pesada"));
-                            break;
-
-                        case 2:
-                            jogador.setArma(new Arma("Sabre", "Leve"));
-                            break;
-
-                        case 3:
-                            jogador.setArma(new Arma("Cajado", "Mágica"));
-                    
-                        default:
-                            break;
-                    }
-                    
                 default:
-                    jogador.exibirAtributos();
-                    System.out.print("\nDeseja confirmar e prosseguir? (S/N)\n> ");
-                    if (input.hasNext("S") || input.hasNext("s")) {
-                        selectedOption = true;
-                        i = 0;
-                        pontos = 50;
-                        input.close();
-                    }
                     break;
             }
-            Console.clear();
-        }
-        System.out.println("Olá, " + jogador.nome + "!");
-        jogador.exibirAtributos();
+        Console.clear();
+        }while(pontos>0);
+        boolean possuirArma = false;
+        do {
+            System.out.println("\t<<<Escolha de arma>>>\n");
+            System.out.println("Nome: " + jogador.nome);
+            System.out.print("1 - Claymore (Pesada)-->Escala com força\n2 - Sabre (Leve)-->Escala com destreza\n3 - Cajado-->Escala com inteligência\n> ");
+            switch (input.nextInt()) {
+                case 1:
+                    jogador.setArma(new Arma("Pesada", 10, "Claymore"));
+                    possuirArma = true;
+                    break;
+
+                case 2:
+                    jogador.setArma(new Arma("leve", 1.2, "sabre"));
+                    possuirArma = true;
+                    break;
+
+                case 3:
+                    jogador.setArma(new Arma("mágica", 1.3, "cajado" ));
+                    possuirArma = true;
+
+                default:
+                    break;
+            }
+
+        }while(possuirArma = false);
+
+        boolean possuirArmadura = false;
+        do{
+            System.out.println("\t<<<Escolha de Armadura>>>\n");
+            System.out.println("Nome: " + jogador.nome);
+            System.out.println("1 - Armadura pesada(Reduz consideravelmente sua agilidade)\n 2 - Armadura leve(Reduz levemente sua agilidade)\n>");
+            switch (input.nextInt()) {
+                case 1:
+                    jogador.setArmadura(new Armadura(6, 3));
+                    possuirArmadura = true;
+                    break;
+                case 2:
+                    jogador.setArmadura(new Armadura(3, 1));
+                    possuirArmadura = true;
+                    break;
+            }
+        }while(possuirArmadura = false);
+
+
+
     }
+
 }
