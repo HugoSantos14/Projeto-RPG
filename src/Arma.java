bb1da2f36baa7f23e199c1b35ef42a67b0498c09
Gabriel <159343630+GabrielEliDias@users.noreply.github.com>
2024-09-23 14:33:52 -0400

    Update Arma.java

--- src/Arma.java
+++ src/Arma.java
@@ -1,7 +1,30 @@
 class Arma {
+    Jogador jogador;
     public String nome;
     public String categoria;
     private double constDano;
+    private int danoPesada;
+    private int danoLeve;
+    Dado d6 = new Dado (6);
+    Dado d4 = new Dado (4);
+    Dado d12 = new Dado (12);
+
+
+    public int getDanoLeve() {
+        return danoLeve;
+    }
+
+    public void setDanoLeve(int danoLeve) {
+        this.danoLeve = (int) (this.getConstDano()+ d6.Lançar()+ d6.Lançar()+ d4.Lançar()+jogador.getDestreza());
+    }
+
+    public int getDanoPesada() {
+        return danoPesada;
+    }
+
+    public void setDanoPesada(int danoPesada) {
+        this.danoPesada = (int) (this.constDano + d12.Lançar() + jogador.getForça()*1.5);
+    }
 
     public String getCategoria() {
         return categoria;
