416e432ee17c5f0ae258aaedcc58190bb2400be3
Hugo Santos <hugosantos14000@gmail.com>
2024-09-23 12:40:30 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,27 +4,26 @@ public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Console.clear();
-        Menu.CriarPersonagem();
-        // while (true) {
-        //     System.out.println("_________ _______  _______  _______      _______  _______      ______   _______ _________ _______ ");
-        //     System.out.println("\\__    _/(  ___  )(  ____ \\(  ___  )    (  ____ \\(       )    (  ___ \\ (  ____ \\\\__   __/(  ___  )");
-        //     System.out.println("   )  (  | (   ) || (    \\/| (   ) |    | (    \\/| () () |    | (   ) )| (    \\/   ) (   | (   ) |");
-        //     System.out.println("   |  |  | |   | || |      | |   | |    | (__    | || || |    | (__/ / | (__       | |   | (___) |");
-        //     System.out.println("   |  |  | |   | || | ____ | |   | |    |  __)   | |(_)| |    |  __ (  |  __)      | |   |  ___  |");
-        //     System.out.println("   |  |  | |   | || | \\_  )| |   | |    | (      | |   | |    | (  \\ \\ | (         | |   | (   ) |");
-        //     System.out.println("|\\_)  )  | (___) || (___) || (___) |    | (____/\\| )   ( |    | )___) )| (____/\\   | |   | )   ( |");
-        //     System.out.println("(____/   (_______)(_______)(_______)    (_______/|/     \\|    |/ \\___/ (_______/   )_(   |/     \\|");
-        //     System.out.println("                                                                                                  ");
+        // Menu.CriarPersonagem();
+        while (true) {
+            System.out.println(" _____  _      _                      _    _            ______                                          ");
+            System.out.println("|  ___|| |    | |                    | |  | |           |  _  \\                                         ");
+            System.out.println("| |__  | |  __| |  ___   _ __   __ _ | |_ | |__         | | | | _   _  _ __    __ _   ___   ___   _ __  ");
+            System.out.println("|  __| | | / _` | / _ \\ | '__| / _` || __|| '_ \\        | | | || | | || '_ \\  / _` | / _ \\ / _ \\ | '_ \\ ");
+            System.out.println("| |___ | || (_| || (_) || |   | (_| || |_ | | | |       | |/ / | |_| || | | || (_| ||  __/| (_) || | | |");
+            System.out.println("\\____/ |_| \\__,_| \\___/ |_|    \\__,_| \\__||_| |_|       |___/   \\__,_||_| |_| \\__, | \\___| \\___/ |_| |_|");
+            System.out.println("                                                                               __/ |                    ");
+            System.out.println("                                                                              |___/                     ");
     
-        //     System.out.println("1 - Jogar\t 2 - Sair\n\n");
-        //     int selectedOption = input.nextInt();
-        //     Menu.LimparConsole();
-        //     if (selectedOption == 1) {
-        //         Menu.CriarPersonagem();
-        //     } else if (selectedOption == 2) {
-        //         break;
-        //     }
-        // }
+            System.out.println("1 - Jogar\t 2 - Sair\n\n");
+            int selectedOption = input.nextInt();
+            Console.clear();
+            if (selectedOption == 1) {
+                Menu.CriarPersonagem();
+            } else if (selectedOption == 2) {
+                break;
+            }
+        }
         input.close();
     }
 }
