9991eadbe7aaadd9825d64dcb989d02b9ec556f8
GabrielEliDias <gabrielelidias@proton.me>
2025-04-05 16:44:49 -0300

    Game logic add

---
+++ src/gamelogic/CharacterLogic.java
@@ -0,0 +1,26 @@
+package gamelogic;
+
+public class CharacterLogic {
+
+    Character character;
+
+    public int Attack(){
+        return 0;
+    }
+
+    public int Defense(){
+        return 0;
+    }
+
+    public boolean hasItem(){
+        return false;
+    }
+
+    public void useItem(){
+
+    }
+
+    public void Heal(){
+
+    }
+}
